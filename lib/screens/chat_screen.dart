f624636b123875e0f48349f2090c3fc18980112a
Ebrahim Hossain <ebrahimhossain626@gmail.com>
2025-09-17 22:40:29 +0600

    updated text

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -87,24 +87,65 @@ class _ChatScreenState extends State<ChatScreen> {
                       final messageText = message.data()['text'];
                       final messageSender = message.data()['sender'];
                       final messageWidget = Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 10,horizontal: 20),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(30),
-                            color: Colors.lightBlueAccent,
-                          ),
-                          padding: EdgeInsetsGeometry.symmetric(vertical: 10,horizontal: 20),
-                          child: Text(
-                            '$messageText from $messageSender',
-                            style: TextStyle(fontSize: 20,color: Colors.white),
-                          ),
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 15,
+                          horizontal: 20,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              padding: EdgeInsetsGeometry.directional(start: 15) ,
+                              child: Text(
+                                '$messageSender',
+                                style: TextStyle(
+                                  color: Colors.black38,
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 2),
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(30),
+                                color: Colors.lightBlueAccent,
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black54,
+                                    spreadRadius: 0.5,
+                                    blurRadius: 8,
+                                    offset: Offset(1, 4),
+                                  ),
+                                ],
+                              ),
+                              padding: EdgeInsetsGeometry.symmetric(
+                                vertical: 10,
+                                horizontal: 20,
+                              ),
+                              child: Text(
+                                '$messageText',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       );
                       messageWidgets.add(messageWidget);
                     }
                     return ListView(
-                      padding: EdgeInsetsGeometry.symmetric(vertical: 10,horizontal: 20),
-                        children: [Column(crossAxisAlignment: CrossAxisAlignment.start,children: messageWidgets,)]
+                      padding: EdgeInsetsGeometry.symmetric(
+                        vertical: 10,
+                        horizontal: 20,
+                      ),
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: messageWidgets,
+                        ),
+                      ],
                     );
                   }
                   return Center(child: CircularProgressIndicator());
