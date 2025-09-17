c729cce828d68da7340a8f80e81fae04dc68df8b
Ebrahim Hossain <ebrahimhossain626@gmail.com>
2025-09-17 22:22:18 +0600

    updated text

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -33,7 +33,7 @@ class _ChatScreenState extends State<ChatScreen> {
         logInUser = user;
         log('${logInUser?.email}');
       }
-    }catch(e){
+    } catch (e) {
       log(e.toString());
     }
   }
@@ -45,8 +45,8 @@ class _ChatScreenState extends State<ChatScreen> {
   //   }
   // }
   Future<void> getSteam() async {
-    await for (var snapShots in _fireStore.collection('messages').snapshots()){
-      for (var message in snapShots.docs){
+    await for (var snapShots in _fireStore.collection('messages').snapshots()) {
+      for (var message in snapShots.docs) {
         log("${message.data()}");
       }
     }
@@ -60,12 +60,13 @@ class _ChatScreenState extends State<ChatScreen> {
         leading: null,
         actions: <Widget>[
           IconButton(
-              icon: Icon(Icons.close),
-              onPressed: () {
-                getSteam();
-                // _auth.signOut();
-                // Navigator.pop(context);
-              }),
+            icon: Icon(Icons.close),
+            onPressed: () {
+              getSteam();
+              // _auth.signOut();
+              // Navigator.pop(context);
+            },
+          ),
         ],
         title: Text('⚡️Chat'),
         backgroundColor: Colors.lightBlueAccent,
@@ -77,23 +78,37 @@ class _ChatScreenState extends State<ChatScreen> {
           children: <Widget>[
             Expanded(
               child: StreamBuilder(
-                  stream: _fireStore.collection('messages').snapshots(),
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      List<Widget> messageWidgets = [];
-                      final messages = snapshot.data?.docs;
-                      for (var message in messages!) {
-                        final messageText = message.data()['text'];
-                        final messageSender = message.data()['sender'];
-                        final messageWidget = Text('$messageText from $messageSender');
-                        messageWidgets.add(messageWidget);
-                      }
-                      return Column(children: messageWidgets,);
+                stream: _fireStore.collection('messages').snapshots(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    List<Widget> messageWidgets = [];
+                    final messages = snapshot.data?.docs;
+                    for (var message in messages!) {
+                      final messageText = message.data()['text'];
+                      final messageSender = message.data()['sender'];
+                      final messageWidget = Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 10,horizontal: 20),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30),
+                            color: Colors.lightBlueAccent,
+                          ),
+                          padding: EdgeInsetsGeometry.symmetric(vertical: 10,horizontal: 20),
+                          child: Text(
+                            '$messageText from $messageSender',
+                            style: TextStyle(fontSize: 20,color: Colors.white),
+                          ),
+                        ),
+                      );
+                      messageWidgets.add(messageWidget);
                     }
-                    return Center(
-                      child: CircularProgressIndicator(),
+                    return ListView(
+                      padding: EdgeInsetsGeometry.symmetric(vertical: 10,horizontal: 20),
+                        children: [Column(crossAxisAlignment: CrossAxisAlignment.start,children: messageWidgets,)]
                     );
-                  },
+                  }
+                  return Center(child: CircularProgressIndicator());
+                },
               ),
             ),
             Container(
@@ -114,19 +129,16 @@ class _ChatScreenState extends State<ChatScreen> {
                     onPressed: () {
                       //Implement send functionality.
                       _fireStore.collection('messages').add({
-                        'sender' : logInUser?.email,
-                        'text' : text,
+                        'sender': logInUser?.email,
+                        'text': text,
                       });
                     },
-                    child: Text(
-                      'Send',
-                      style: kSendButtonTextStyle,
-                    ),
+                    child: Text('Send', style: kSendButtonTextStyle),
                   ),
                 ],
               ),
             ),
-            SizedBox(height: 20,)
+            SizedBox(height: 20),
           ],
         ),
       ),
